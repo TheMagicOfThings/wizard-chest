b608ceeaa1b11c0e762900161e32ae6cc7cdda2a
lyudmilatanasov <45456962+lyudmilatanasov@users.noreply.github.com>
2020-05-21 17:10:34 +0100

    Update wizard-chest.ino

--- wizard-chest/wizard-chest.ino
+++ wizard-chest/wizard-chest.ino
@@ -6,11 +6,13 @@
  * 
  * Written by The Magic Of Things
  *************************************************************************/
-
+#include <EEPROM.h>
 
 // Arduino pin connected to the lock
 #define lockPin            3
 
+// Arduino pin connected to the keyhole led
+#define keyholeLED            10
 
 int sensorPin[3] = {5, 6, 7};           // Arduino pin connected to the Hall effect sensors
 int sensorReading[3];                   // reading from the sensor pin
@@ -18,39 +20,53 @@ int currentState[3] = {1, 1, 1};        // the current state of the output pin
 int previousSate[3] = {0, 0, 0};        // the previousSate reading from the input pin
 
 int input[4] = {0, 0, 0, 0};
-int pass[4] = {1, 2, 3, 1};
+int pass[4] = {0, 0, 0, 0};
+
+bool defaultPass = 1;
 
 // Variables used for debouncing
 long time = 0;         // the last time the output pin was toggled
 long debounce = 100;   // delay between readings
+
 
 void setup() {
   //Initialise the sensor pins as an input with a pullup resistor
   pinMode(sensorPin[0], INPUT_PULLUP);
   pinMode(sensorPin[1], INPUT_PULLUP);
   pinMode(sensorPin[2], INPUT_PULLUP);
+  //Initialise the lock and led pins as an output
   pinMode(lockPin, OUTPUT);
+  pinMode(keyholeLED, OUTPUT);
   digitalWrite(lockPin, LOW);
+  digitalWrite(keyholeLED, LOW);
   Serial.begin(9600);
+
+  //hold the wand at the first touch point during start up to enter menu mode
+  sensorReading[0] = digitalRead(sensorPin[0]); //Get a reading from the sensor
+  if(sensorReading[0]==0){
+    menuMode();
+    
+    }
+
+    //Read the password stored in EEPROM 
+    pass[0] = EEPROM.read(0);
+    pass[1] = EEPROM.read(1);
+    pass[2] = EEPROM.read(2);
+    pass[3] = EEPROM.read(3);
+    
 
 }
 
 void loop() {
-
-for(int j=0; j<4; j++)
-{
-  Serial.println(input[j]);
-}
-Serial.println(" ");
-delay(200);
 
   checkState();
   checkCombination();
   
 }
 
+
+//Update the rray that stores the password and shift all values left
 void shiftArray(){
-  
   int tmp;
   int *p = &input[0];
   int *q = &input[1];
@@ -89,11 +105,14 @@ void checkCombination(){
   
   //check if the input array matches the unlock code  
   if(input[0] == pass[0] && input[1] == pass[1] && input[2] == pass[2] && input[3] == pass[3]){
-    digitalWrite(lockPin, HIGH);
-    delay(300);
-    digitalWrite(lockPin, LOW);
     
-    //reset the input array
+    digitalWrite(lockPin, HIGH);
+    digitalWrite(keyholeLED, HIGH);
+    delay(1000);                  //it takes 0.8s for the lock to trigger
+    digitalWrite(lockPin, LOW);   //turn the lock back off
+    delay(1000);                  // keep the led on for a bit longer after unlock
+    digitalWrite(keyholeLED, LOW);//turn the led off
+    //reset the input array after successful code entry
     input[0] = 0;
     input[1] = 0;
     input[2] = 0;
@@ -101,5 +120,33 @@ void checkCombination(){
    }
     else{
       digitalWrite(lockPin, LOW);
+    }
+}
+
+void menuMode(){
+  flashLED();
+  //run the check state function untill all four entries are filled
+  while(input[0]== 0){
+  checkState();
+  }
+  for(int j=0; j<4; j++){
+  pass[j] = input[j];
+  }
+  
+  //Store the new password in EEPROM
+  EEPROM.write(0, pass[0]);
+  EEPROM.write(1, pass[1]);
+  EEPROM.write(2, pass[2]);
+  EEPROM.write(3, pass[3]);
+}
+
+
+//Flash the LED few time to indicate menue mode
+void flashLED(){
+    for(int i=0; i<5; i++){
+      digitalWrite(keyholeLED, HIGH);
+      delay(400);
+      digitalWrite(keyholeLED, LOW);
+      delay(200);
     }
 }
